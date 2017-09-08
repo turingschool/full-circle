4e27526da576d7fb0c7a456e33abf52feea9477d
Aaron Olsen <mraaronolsen@gmail.com>
2017-09-08 11:32:59 -0600

    User Migration

---
+++ db/migrate/20170908172936_create_users_table.rb
@@ -0,0 +1,13 @@
+class CreateUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users_tables do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :github_id
+      t.text :github_token
+      t.integer :role default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
