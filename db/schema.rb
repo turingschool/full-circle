5fc904111d18acfce9e5ad0789df251c16fb6fc6
Aaron Olsen <mraaronolsen@gmail.com>
2017-09-10 14:14:46 -0600

    Cohort Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908172936) do
+ActiveRecord::Schema.define(version: 20170910201306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string "title"
+    t.date "start_date"
+    t.date "end_time"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
