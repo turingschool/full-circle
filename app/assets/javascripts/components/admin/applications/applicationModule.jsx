ac3368efe9b57322f94d292ced78dd5865eafcb9
Aaron Olsen <mraaronolsen@gmail.com>
2017-09-11 00:09:43 -0600

    Applications color coded by status

--- app/assets/javascripts/components/admin/applications/applicationModule.jsx
+++ app/assets/javascripts/components/admin/applications/applicationModule.jsx
@@ -2,7 +2,7 @@ class ApplicationModule extends React.Component {
 
   render() {
     return (
-      <div className={['application']}>
+      <div className={ ['application', this.props.app.status].join(' ') }>
         <span>{this.props.app.id}</span>
         <span>{this.props.app.status}</span>
       </div>
