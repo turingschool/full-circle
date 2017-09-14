e3739af381d4d620a64512291421e832c59403da
Aaron Olsen <mraaronolsen@gmail.com>
2017-09-14 14:00:00 -0600

    React testing is working with initial oauth user login mocks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
-# Capybara
 require 'capybara/rails'
-# Require shoulda-matchers and config it with Rails and RSpec
 require 'shoulda-matchers'
+require 'capybara/poltergeist'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -16,6 +17,28 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+    js_errors: false,
+    phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=any'],
+    debug: false,
+    timeout: 500,
+    phantomjs: File.absolute_path(Phantomjs.path)
+  })
+end
+
+Capybara.javascript_driver = :poltergeist
+Capybara.server_port = 3001
+
+OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+  uid: '90210',
+  credentials: { token: 'averysecrettoken' },
+  info: {
+    name: 'John Galt',
+    email: 'somewhere@colorado.com'
+  }
+})
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
