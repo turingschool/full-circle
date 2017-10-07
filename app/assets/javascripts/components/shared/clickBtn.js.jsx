bad71a334ec91cd57dacb3cb6d0c8b982b6fc3a1
Aaron Olsen <mraaronolsen@gmail.com>
2017-10-07 07:50:31 -0400

    Disable function to clickbtn

--- app/assets/javascripts/components/shared/clickBtn.js.jsx
+++ app/assets/javascripts/components/shared/clickBtn.js.jsx
@@ -1,7 +1,20 @@
 class ClickBtn extends React.Component {
+  constructor(props) {
+    super(props)
+
+    if (this.props.disabled) {
+      this.disabled = true
+    } else {
+      this.disabled = false
+    }
+  }
+
   render() {
     return(
-      <div className='btn' onClick={this.props.onClick}>
+      <div className='btn'
+        onClick={this.props.onClick}
+        disabled={this.disabled}>
+
         {this.props.Text}
       </div>
     )
