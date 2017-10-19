77c93993590767da255f4d3a50296d602ce4496d
Aaron Olsen <mraaronolsen@gmail.com>
2017-10-18 18:51:38 -0600

    Makes app view take up proper screen width

--- app/assets/javascripts/components/admin/view_cohorts_page/view_application/adminViewApplicationSection.js.jsx
+++ app/assets/javascripts/components/admin/view_cohorts_page/view_application/adminViewApplicationSection.js.jsx
@@ -2,7 +2,7 @@ class AdminViewApplicationSection extends React.Component {
 
   render() {
     return(
-      <section className='view-application'>
+      <section className='view-applications'>
         Application
       </section>
     )
