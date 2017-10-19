e7f1da0c8cf18646e7b706f4ee14b49bf9bfcdf1
Aaron Olsen <mraaronolsen@gmail.com>
2017-10-18 20:42:18 -0600

    Adding Applications List

--- app/assets/javascripts/components/admin/view_cohorts_page/view_cohorts/adminCohortApplications.js.jsx
+++ app/assets/javascripts/components/admin/view_cohorts_page/view_cohorts/adminCohortApplications.js.jsx
@@ -3,7 +3,15 @@ class AdminCohortApplications extends React.Component {
   render() {
     return(
       <section className='cohort-applications'>
-        Applications!
+        <StaticTextField
+          label='Total Apps'
+          text={this.props.cohort.applications.length}
+          width='50%'/>
+
+        <AdminCohortApplicationList
+          applications={this.props.cohort.applications}
+          handleAction={this.props.handleAction} />
+        
       </section>
     )
   }
---
+++ app/assets/javascripts/components/admin/view_cohorts_page/view_cohorts/adminCohortApplicationsList.js.jsx
@@ -0,0 +1,12 @@
+class AdminCohortApplicationList extends React.Component {
+
+  render() {
+    return(
+      <section className='cohort-applications-list'>
+        {this.props.applications.map((app) => {
+          debugger
+        })}
+      </section>
+    )
+  }
+}
