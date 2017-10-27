5703b7b11a36a1c2a2a0fd84ede81ef27e4683a9
Aaron Olsen <mraaronolsen@gmail.com>
2017-10-27 06:36:50 -0600

    Fixed student page crashing when no curent cohort but user has past application

--- app/controllers/student/dashboard_controller.rb
+++ app/controllers/student/dashboard_controller.rb
@@ -3,14 +3,17 @@ class Student::DashboardController < ApplicationController
   def index
     @cohort = Cohort.current[0]
     @user = current_user
-
-    if @user.application && @cohort.applications.find(@user.application.id)
-      @application = @cohort.applications.find(@user.application.id)
-    else
-      @application = false
-    end
+    @application = get_application
 
     @authorization = JwToken.encode({user_id: current_user.id})
+  end
+
+  def get_application
+    if @cohort && @user.application
+      @cohort.applications.find(@user.application.id) || false
+    else
+      false
+    end
   end
 
 end
