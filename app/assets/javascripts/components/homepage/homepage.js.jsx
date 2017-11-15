df7999aa8cd7d5a56c82bd80817072145d113595
VictoriaVasys <vvasys@gmail.com>
2017-11-15 09:50:00 -0700

    Add homepage header

--- app/assets/javascripts/components/homepage/homepage.js.jsx
+++ app/assets/javascripts/components/homepage/homepage.js.jsx
@@ -5,6 +5,9 @@ class HomePage extends React.Component {
       <main className='main-vert-frame'>
         <Header />
         <section className='homepage-frame'>
+          <h3>Turing School of Software & Design</h3>
+          <h2>Scholarship Application</h2>
+          <br></br>
           <LinkBtn Text='Login' Url='/auth/github' />
         </section>
       </main>
