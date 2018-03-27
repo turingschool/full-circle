3db8810eff48f6979bcc5b5d3801145d57c71c0d
Simon Taranto <simon.taranto@gmail.com>
2018-03-27 13:57:05 -0600

    Moving things around wip

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,16 @@ class Admin::DashboardController < AdminController
 
   def index
     # TODO(srt32): add serialization layer here
-    @cohorts = Cohort.all.to_json(:include => [{:applications => {:include => [:user, :reviews]}}, :reviewers, :non_reviewers], methods: [:formatted_close_date, :formatted_start_date, :formatted_notify_date])
+    @cohorts = Cohort.all.to_json(
+      :include => [
+        {
+          applications: { include: [:user, :reviews] }
+        },
+        :reviewers,
+        :non_reviewers
+      ],
+      methods: [:formatted_close_date, :formatted_start_date, :formatted_notify_date]
+    )
     @users = User.all.to_json
     @user = current_user
     @authorization = JwToken.encode({user_id: current_user.id})
