8ed01e3f74f1a8439c94f8078251064abd7bec3c
VictoriaVasys <vvasys@gmail.com>
2018-04-23 19:12:41 -0600

    Update logic to pass correct cohort prop

--- app/assets/javascripts/components/reviewer/reviewerDashboard.js.jsx
+++ app/assets/javascripts/components/reviewer/reviewerDashboard.js.jsx
@@ -4,17 +4,17 @@ class ReviewerDashboard extends React.Component {
     super(props)
 
     this.cohorts = JSON.parse(this.props.cohorts)
-    this.current_cohort = JSON.parse(this.props.current_cohort)
+    if(this.current_cohort){this.current_cohort = JSON.parse(this.props.current_cohort)}
     this.defineCurrentCohort()
-    this.current_cohort.title = this.current_cohort.title + ' (Current Cohort)'
+    if(this.current_cohort){this.current_cohort.title + ' (Current Cohort)'}
     this.user = JSON.parse(this.props.user)
     this.authorization = 'Bearer ' + this.props.authorization
   }
   
   defineCurrentCohort() {
     return this.cohorts.map((cohort, i) => {
-      if (cohort.title == this.current_cohort.title) {
-        cohort.title = this.current_cohort.title + ' (Current Cohort)'
+      if (this.current_cohort && cohort.title === this.current_cohort.title) {
+        cohort.title = cohort.title + ' (Current Cohort)'
       }
     })
   }
@@ -23,19 +23,25 @@ class ReviewerDashboard extends React.Component {
     this.setState(action)
   }
   
+  findCohort(){
+    if(this.current_cohort) {
+      return this.current_cohort
+    } else {
+      return this.cohorts[0]
+    }
+  }
+  
   confirmReviewerHasCohorts() {
-    if(this.user.) {
+    if(this.user.cohort_reviewers.length != 0) {
       return (<ReviewerViewApplications
         cohorts={this.cohorts}
         user={this.user}
-        cohort={this.current_cohort}
+        cohort={this.findCohort()}
         message={this.message}
         authorization={this.authorization} />)
     } else {
-      return (<section> Sorry, you do not have any Cohorts to review </section>)
+      return (<section className='reviewer-header'>You do not yet have any Cohorts to review</section>)
     }
-    
-    
   }
   
   render() {
