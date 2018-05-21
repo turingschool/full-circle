8ba03945237bf86bfdc44fce6ec9a1662287da22
VictoriaVasys <vvasys@gmail.com>
2018-05-21 01:40:24 -0600

    Better alignment for admin cohort apps list

--- app/assets/javascripts/components/admin/view_cohorts_page/view_cohorts/adminCohortApplicationsList.js.jsx
+++ app/assets/javascripts/components/admin/view_cohorts_page/view_cohorts/adminCohortApplicationsList.js.jsx
@@ -6,7 +6,7 @@ class AdminCohortApplicationList extends React.Component {
         {this.props.applications.map((app) => {
           return <SelectableTextField
             key={app.user.id}
-            texts={[app.user.email, app.state, app.id]}
+            texts={[app.id, app.user.email, app.state]}
             width='100%'
             returnKey='application'
             returnValue={app}
