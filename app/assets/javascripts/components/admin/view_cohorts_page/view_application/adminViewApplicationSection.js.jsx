2648ec76bd31e45ee6da867a512ab97c1f0415de
Katelyn Kasperowicz <kat3kasper@gmail.com>
2018-10-19 14:09:40 +0200

    Only show action bar if status undecided

--- app/assets/javascripts/components/admin/view_cohorts_page/view_application/adminViewApplicationSection.js.jsx
+++ app/assets/javascripts/components/admin/view_cohorts_page/view_application/adminViewApplicationSection.js.jsx
@@ -5,9 +5,13 @@ class AdminViewApplicationSection extends React.Component {
       <section className='view-applications'>
         <h4 className='reviewer-header'>{this.props.application.user.alt_name}'s Application</h4>
         <h5>{this.props.application.user.alt_name.split(' ')[0]}'s Email: {this.props.application.user.email}</h5>
-        <AdminApplicationActionBar
-          application={this.props.application}
-          handleAction={this.props.handleAction} />
+        { this.props.application.status === "undecided" ? (
+          <AdminApplicationActionBar
+            application={this.props.application}
+            handleAction={this.props.handleAction} />
+        ) : (
+          <h5>Status: {this.props.application.status}</h5>
+        )}
 
         <AdminAppDataSection
           application={this.props.application} />
