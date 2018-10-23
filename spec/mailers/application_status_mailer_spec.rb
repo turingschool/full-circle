e5d3ff46ce39662077c3df68820e7c9ad01eaf77
Katelyn Kasperowicz <kat3kasper@gmail.com>
2018-10-23 13:55:46 +0200

    Update tests for dynamic cohort creation

--- spec/mailers/application_status_mailer_spec.rb
+++ spec/mailers/application_status_mailer_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe ApplicationStatusMailer do
       assert_equal ['contact@turing.io'], email.from
       assert_equal ['somewhere@colorado.com'], email.to
 
-      award_email_text = "Dear John Galt\n\nCongratulations! I'm writing to let you know that you have been selected as a scholarship recipient of our 171 Turing School of Software & Design Diversity Scholarship. You’ll soon receive a credit agreement through Right Signature reflecting the additional $4,000 tuition waiver.\n\nWe are looking forward to having you here at Turing next month, and we are excited to be part of this next stage in your journey.\n\nThank you for sharing your story with us. We hope this helps you achieve your dreams.\n\n"
+      award_email_text = "Dear John Galt\n\nCongratulations! I'm writing to let you know that you have been selected as a scholarship recipient of our #{application.cohort.title} Turing School of Software & Design Diversity Scholarship. You’ll soon receive a credit agreement through Right Signature reflecting the additional $4,000 tuition waiver.\n\nWe are looking forward to having you here at Turing next month, and we are excited to be part of this next stage in your journey.\n\nThank you for sharing your story with us. We hope this helps you achieve your dreams.\n\n"
       assert_equal award_email_text, email.text_part.body.to_s
     end
 
@@ -40,7 +40,7 @@ RSpec.describe ApplicationStatusMailer do
       assert_equal ['contact@turing.io'], email.from
       assert_equal ['somewhere@colorado.com'], email.to
 
-      decline_email_text = "Dear John Galt\n\nI’d like to thank you for applying for the 171 Turing School of Software & Design Diversity Scholarship. I regret to inform you that you were not selected to receive an award.\n\nTuring School received many strong applications this round. We had a very difficult time selecting scholarship recipients for this cohort because of the passion and need of each candidate.\n\nWe are looking forward to having you here at Turing next month, and we are excited to be part of this next stage in your journey.\n\nThank you for sharing your story with us. We look forward to helping you achieve your dreams.\n\n"
+      decline_email_text = "Dear John Galt\n\nI’d like to thank you for applying for the #{application.cohort.title} Turing School of Software & Design Diversity Scholarship. I regret to inform you that you were not selected to receive an award.\n\nTuring School received many strong applications this round. We had a very difficult time selecting scholarship recipients for this cohort because of the passion and need of each candidate.\n\nWe are looking forward to having you here at Turing next month, and we are excited to be part of this next stage in your journey.\n\nThank you for sharing your story with us. We look forward to helping you achieve your dreams.\n\n"
       assert_equal decline_email_text, email.text_part.body.to_s
     end
   end
