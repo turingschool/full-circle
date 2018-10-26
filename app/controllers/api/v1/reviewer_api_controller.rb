58fd9afdf218eebb4122bbe625d2aaa5ec429328
Katelyn Kasperowicz <kat3kasper@gmail.com>
2018-10-26 15:00:50 +0200

    Update tests to reflect new error messaging

--- app/controllers/api/v1/reviewer_api_controller.rb
+++ app/controllers/api/v1/reviewer_api_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::ReviewerApiController < Api::V1::ApiController
   before_action :authorize_requester
 
   def authorize_requester
-    render json: { "error"=>"Forbidden" }, status: 403 unless current_requester.reviewer?
+    unless current_requester.reviewer?
+      Honeybadger.notify("Api::V1::ReviewerApiController - Forbidden")
+      render json: { "error"=>"Api::V1::ReviewerApiController - Forbidden" }, status: 403
+    end
   end
 end
