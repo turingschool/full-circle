52762807afd0b8d789492f22a2df03027951e31b
Katelyn Kasperowicz <kat3kasper@gmail.com>
2018-10-26 14:51:47 +0200

    Add additional information for auth errors with Honeybadger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   include Authorize
 
+  before_action do
+    Honeybadger.context({
+      user_id: current_user.id,
+      user_email: current_user.email
+    })
+  end
+
   protect_from_forgery with: :exception
   helper_method :current_user
 
