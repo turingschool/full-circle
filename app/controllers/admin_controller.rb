fc17ee775b5c519d1f5a5004f72e109b3a7249ab
Katelyn Kasperowicz <kat3kasper@gmail.com>
2018-10-29 16:09:49 +0100

    Check for current_user before authorizing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,3 @@
 class AdminController < ApplicationController
-  before_action :authorize!
-
-  private
-
-    def authorize!
-      unless admin?
-        Honeybadger.notify("Reviewer::AdminController - Unauthorized")
-        render file: "/public/404"
-      end
-    end
+  before_action :authorize_admin!
 end
--- app/controllers/reviewer/base_controller.rb
+++ app/controllers/reviewer/base_controller.rb
@@ -1,12 +1,3 @@
 class Reviewer::BaseController < ApplicationController
-  before_action :authorize!
-
-  private
-
-    def authorize!
-      unless admin? || reviewer?
-        Honeybadger.notify("Reviewer::BaseController - Unauthorized")
-        render file: "/public/404"
-      end
-    end
+  before_action :authorize_reviewer!
 end
