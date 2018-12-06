ae029c860475f69fb047fad205fcdf1adb3bf06d
Katelyn Kasperowicz <kat3kasper@gmail.com>
2018-12-06 09:24:55 -0700

    Spell Scholarship correctly

--- app/assets/javascripts/components/student/decisions/notAcceptingApplications.js.jsx
+++ app/assets/javascripts/components/student/decisions/notAcceptingApplications.js.jsx
@@ -3,7 +3,7 @@ class NotAcceptingApplications extends React.Component {
   render() {
     return (
       <section className="empty-container">
-        <h1>Turing is Currently Not Accepting Diversity Scholorship Applications</h1>
+        <h1>Turing is Currently Not Accepting Diversity Scholarship Applications</h1>
       </section>
     )
   }
